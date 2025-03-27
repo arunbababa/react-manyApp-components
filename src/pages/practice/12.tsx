c7b833a68b1b2f02a6c93f9216e341b1bc402a2a
arunbababa <hatuki.1.gzs@icloud.com>
2025-03-27 23:08:59 +0900

    カスタムフック化前の実装

--- src/pages/practice/12.tsx
+++ src/pages/practice/12.tsx
@@ -10,13 +10,13 @@ const Page: NextPage = () => {
 
   // うわー！西本さんと同じ流れでここまでこれたのは嬉しい丁度同じtimeIDのスコープが違うのでいい感じにできぬ...(ある意味ないス)
   useEffect(() => {
-    // NodeJS.Timeout：Node.js 環境で setInterval() が返す「タイマーIDの型」
+    // NodeJS.Timeout：Node.js 環境で setInterval() が返す「タイマーIDの型」、もしくは初回レンダリング用の型定義でundefined、なお= undefinedとするのは初期値として明治して読みやすくしている感じ
     let interval: NodeJS.Timeout | undefined = undefined;
     if (isActive) {
       interval = setInterval(() => {
         setTime((prev) => prev + 1);
       }, 1000);
-      // あぁあれかsecondsがゼロじゃないっていうのはスタートしてタイマーが例えば5秒の時とかをリセットしたいからか、んで0秒の時は別にリセットする必要ないからね
+      // あぁあれかsecondsがゼロじゃないっていうのはスタートしてタイマーが例えば5秒の時とかに押してストップしたいからかclearintervalで足し算を止めることができるのだ、んで0秒の時は別にストップする必要ないからねだからseconds !== 0としている
     } else if (!isActive && seconds !== 0) {
       clearInterval(interval);
     }
