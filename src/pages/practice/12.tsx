01cf309235ad7c22c30f100673c0decf04e076db
arunbababa <hatuki.1.gzs@icloud.com>
2025-03-27 22:13:12 +0900

    一旦完成理解深めるコメントをこの後追記していく

--- src/pages/practice/12.tsx
+++ src/pages/practice/12.tsx
@@ -5,34 +5,48 @@ import Button from '@/components/common/parts/Button';
 
 // これめちゃくちゃ練習し甲斐がある
 const Page: NextPage = () => {
-  const [time, setTime] = useState(0);
-  const [timerState, settimerState] = useState(true);
-  const handleTimer = () => {
-    settimerState((prev) => !prev);
-  };
+  const [seconds, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  // うわー！西本さんと同じ流れでここまでこれたのは嬉しい丁度同じtimeIDのスコープが違うのでいい感じにできぬ...(ある意味ないス)
   useEffect(() => {
-    if (timerState === true) {
-      setInterval(() => {
+    // NodeJS.Timeout：Node.js 環境で setInterval() が返す「タイマーIDの型」
+    let interval: NodeJS.Timeout | undefined = undefined;
+    if (isActive) {
+      interval = setInterval(() => {
         setTime((prev) => prev + 1);
       }, 1000);
+      // あぁあれかsecondsがゼロじゃないっていうのはスタートしてタイマーが例えば5秒の時とかをリセットしたいからか、んで0秒の時は別にリセットする必要ないからね
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
     }
-  }, [timerState]);
-  const onResetTimer = () => {};
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const handleClickToggle = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  const handleClickReset = () => {
+    setTime(0);
+    setIsActive(false);
+  };
 
   return (
     <>
       <div className="mx-auto max-w-4xl">
         <div className="mt-10 flex justify-center gap-x-8">
           <div className="">
-            <h3 className="">時間:{time}</h3>
+            <h3 className="">時間:{seconds}</h3>
           </div>
           <div className="ml-10 flex gap-x-8">
             <Button
               variant="text"
-              label={timerState ? 'スタート' : 'ストップ'}
-              onClick={handleTimer}
+              label={isActive ? 'ストップ' : 'スタート'}
+              onClick={handleClickToggle}
             ></Button>
-            <Button variant="text" label="リセット" onClick={onResetTimer}></Button>
+            <Button variant="text" label="リセット" onClick={handleClickReset}></Button>
           </div>
         </div>
       </div>
